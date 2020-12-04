16c3251c0163006a2e00aa081ad18d02868766a9
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 13:57:17 +0100

    v1.0.2

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { LuaArray } from "@wowts/lua";
-export declare function concat<T>(t: LuaArray<T>, seperator?: string): string;
-export declare function insert<T>(t: LuaArray<T>, indexOrValue: number | T, value?: T): void;
-export declare function sort<T>(t: LuaArray<T>, compare?: (left: T, right: T) => boolean): void;
-export declare function remove<T>(t: LuaArray<T>, index?: number): T;
