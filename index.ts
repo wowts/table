ab9dbfa27cd0be8ad2ff5cac77fcd40f60ce92af
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 13:54:58 +0100

    Fix some bugs

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { LuaArray, wipe, lualength } from "@wowts/lua";
 
-export function concat<T>(t:LuaArray<T>, seperator?: string):string {
+export function concat<T extends { toString(): string }>(
+    t: LuaArray<T>,
+    seperator?: string
+): string {
     const result: string[] = [];
     for (let i = 1; t[i] !== undefined; i++) {
         result.push(t[i].toString());
@@ -9,30 +12,31 @@ export function concat<T>(t:LuaArray<T>, seperator?: string):string {
     return result.join(seperator || "");
 }
 
-export function insert<T>(t:LuaArray<T>, indexOrValue:number|T, value?: T) {
+export function insert<T>(t: LuaArray<T>, indexOrValue: number | T, value?: T) {
     const l = lualength(t);
-    if (typeof(indexOrValue) === "number") {
-        for (let i = l; i >= indexOrValue; i --){
-            t[i + 1] = t[i]
+    if (typeof indexOrValue === "number") {
+        for (let i = l; i >= indexOrValue; i--) {
+            t[i + 1] = t[i];
         }
         if (value) t[indexOrValue] = value;
-    }
-    else {
+    } else {
         t[l + 1] = indexOrValue;
     }
 }
 
-export function sort<T>(t:LuaArray<T>, compare?: (left:T,right:T) => boolean) {
-    let values:T[] = [];
+export function sort<T>(
+    t: LuaArray<T>,
+    compare?: (left: T, right: T) => boolean
+) {
+    let values: T[] = [];
     for (let i = 1; ; i++) {
         if (!t[i]) break;
-        values.push(t[i])
+        values.push(t[i]);
     }
     wipe(t);
     if (compare) {
-        values = values.sort((a, b) => a == b ? 0 : (compare(a,b) ? 1 : -1));
-    }
-    else {
+        values = values.sort((a, b) => (a == b ? 0 : compare(a, b) ? 1 : -1));
+    } else {
         values = values.sort();
     }
     for (let i = 0; i < values.length; i++) {
@@ -40,7 +44,7 @@ export function sort<T>(t:LuaArray<T>, compare?: (left:T,right:T) => boolean) {
     }
 }
 
-export function remove<T>(t: LuaArray<T>, index?: number):T {
+export function remove<T>(t: LuaArray<T>, index?: number): T {
     const length = lualength(t);
     const i = index || length;
     const value = t[i];
